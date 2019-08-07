7e7cf5e57d741e5d1668506e3120cf0549ff01c8
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-08-07 11:20:57 +0100

    Updated tests for forecast summary

--- __tests__/forecast-summary.test.jsx
+++ __tests__/forecast-summary.test.jsx
@@ -1,38 +1,34 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
 import ForecastSummary from '../src/components/forecast-summary';
 
 
 describe('<ForecastSummary/>', () => {
-  it('renders the passed date in a span tag', () => {
-    const wrapper = Enzyme.shallow((
-      <ForecastSummary date="1" />
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow((
+      <ForecastSummary
+        date="mockDate"
+        temperature="mockTemperature"
+        description="mockDescription"
+        icon="mockIcon"
+      />
     ));
-    const text = wrapper.find('span').text();
-    expect(text).toEqual('1');
   });
 
-  it('renders the passed temperature in a span tag', () => {
-    const wrapper = Enzyme.shallow((
-      <ForecastSummary temperature="23" />
-    ));
-    const text = wrapper.find('span').text();
-    expect(text).toEqual('23');
+  it('renders the passed date', () => {
+    expect(wrapper.find('.forecast-summary__date').text().toEqual('mockDate'));
   });
 
-  it('renders the passed description in a span tag', () => {
-    const wrapper = Enzyme.shallow((
-      <ForecastSummary description="cloudy" />
-    ));
-    const text = wrapper.find('span').text();
-    expect(text).toEqual('cloudy');
+  it('renders the passed temperature', () => {
+    expect(wrapper.find('forecast-summary__temperature').text().toEqual('mockTemperature'));
   });
 
-  it('renders the passed icon in a span tag', () => {
-    const wrapper = Enzyme.shallow((
-      <ForecastSummary icon = "test" />
-    ));
-    const text = wrapper.find('span').text();
-    expect(text).toEqual('test'); 
+  it('renders the passed description', () => {
+    expect(wrapper.find('.forecast-summary__description').text().toEqual('mockDescription'));
+  });
+
+  it('renders the passed icon', () => {
+    expect(wrapper.find('.forecast-summary__icon').text().toEqual('mockIcon'));
   });
 });
