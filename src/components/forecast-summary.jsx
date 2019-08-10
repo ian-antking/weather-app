47c66c26f954a379142ac4accbd1ea519a1fdfa4
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-08-10 12:45:17 +0100

    Rendered weather icon & formatted date using moment

--- src/components/forecast-summary.jsx
+++ src/components/forecast-summary.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import WeatherIcon from 'react-icons-weather';
+import moment from 'moment';
 
 const ForecastSummary = props => {
   return (
     <div>
-      <span className="date">{props.date}</span>;
-      <br />
-      <span className="icon">{props.icon}</span>
-      <br />
+      <span className="date">{moment(props.date).format('ddd Do MMM')}</span>;
+      <div className="icon">
+        <WeatherIcon name="owm" iconId={props.icon} />
+      </div>
       <span className="temperature">{props.temperature}°c</span>
       <br />
       <span className="description">{props.description}</span>
